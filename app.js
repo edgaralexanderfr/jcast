4628468bbf04b3be434cc182d23da0253343fce6
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2021-06-04 19:30:22 -0500

    Install and create express server.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = process.env.PORT || 9999;
+
+const app = express();
+app.use('/lib', express.static('lib'));
+app.use('/dev', express.static('dev'));
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`App running at port ${port}.`);
+});
