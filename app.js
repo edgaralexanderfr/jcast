8316efa6ed077c0a2f041ebcd5f4ab840b2c32d1
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2021-06-28 13:27:41 -0500

    Update and fix links.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const port = process.env.PORT || 9999;
 
 const app = express();
-app.use('/lib', express.static('lib'));
-app.use('/dev', express.static('dev'));
-app.use('/examples', express.static('examples'))
-app.use(express.static('public'));
+app.use('/jcast/lib', express.static('lib'));
+app.use('/jcast/dev', express.static('dev'));
+app.use('/jcast/examples', express.static('examples'))
+app.use('/jcast', express.static('public'));
 
 app.listen(port, () => {
   console.log(`App running at port ${port}.`);
